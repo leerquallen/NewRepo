29e3efeed8079b93b8248623eaa6e70b539f41d7
leerquallen <user@A26COMP3>
2018-01-31 12:54:39 +0200

    1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//#include <iomanip>
+//#include  <conio.h>
+using namespace std;
+
+int NotMain(int, int);
+
+void main()
+{
+	int num1, num2;
+	cout << "num 1: ";
+	cin >> num1;
+	cout << "num 2: ";
+	cin >> num2;
+
+	cout << "\nsum = " << NotMain(num1, num2) << endl;
+
+	system("pause");
+}
+
+int NotMain(int num1, int num2)
+{
+	int sum = 0;
+	if (num1 < num2)
+	{
+		for (int i = num1 + 1; i < num2; i++)
+		{
+			cout << sum << " + " << i << " = ";
+			sum += i;
+			cout << sum << endl;
+		}
+	}
+	if (num1 > num2)
+	{
+		for (int i = num2 + 1; i < num1; i++)
+		{
+			cout << sum << " + " << i << " = ";
+			sum += i;
+			cout << sum << endl;
+		}
+	}
+	return sum;
+}
